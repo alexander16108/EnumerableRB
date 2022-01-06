ef74e7323c765a8290bda996e05199ec6d8892ce
alexander16108 <alexandermayowa05@gmail.com>
2022-01-06 16:35:23 +0100

    fixing linter errors

--- enumerable.rb
+++ enumerable.rb
@@ -1,13 +1,15 @@
 module Enumerable
-    def all?
-      each { |n| return false unless yield(n) }
-    end
-    def any?
-    each { |n| return true if yield(n) }
-      false
-    end
-      def filter
-        arr = []
-        each { |n| arr.push(n) if yield(n) }
-      end
+  def all?
+    each { |n| return false unless yield(n) }
   end
+
+  def any?
+    each { |n| return true if yield(n) }
+    false
+  end
+
+  def filter
+    arr = []
+    each { |n| arr.push(n) if yield(n) }
+  end
+end
