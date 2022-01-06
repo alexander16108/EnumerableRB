abe0e9cffbb896049f5689d46ed4a9b14bb59cb0
Alexander <60612329+alexander16108@users.noreply.github.com>
2022-01-06 16:42:20 +0100

    Revert "Add module enumerable"

--- enumerable.rb
+++ enumerable.rb
@@ -1,15 +1,13 @@
 module Enumerable
-  def all?
-    each { |n| return false unless yield(n) }
+    def all?
+        each { |n| return false unless yield(n) }
+    end
+    def any?
+        each { |n| return true if yield(n) }
+        false
+    end
+    def filter
+        arr = []
+        each { |n| arr.push(n) if yield(n) }
+    end
   end
-
-  def any?
-    each { |n| return true if yield(n) }
-    false
-  end
-
-  def filter
-    arr = []
-    each { |n| arr.push(n) if yield(n) }
-  end
-end
