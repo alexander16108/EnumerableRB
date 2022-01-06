d648652f1b3e22b28a7f18a4618fe6036addeb15
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-01-06 16:28:48 +0100

    Add module enumerable

---
+++ enumerable.rb
@@ -0,0 +1,13 @@
+module Enumerable
+    def all?
+      each { |n| return false unless yield(n) }
+    end
+    def any?
+    each { |n| return true if yield(n) }
+      false
+    end
+      def filter
+        arr = []
+        each { |n| arr.push(n) if yield(n) }
+      end
+  end
